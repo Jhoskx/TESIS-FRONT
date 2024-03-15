2593f8d25ac67398b8ea9fbf0086e8382d48dfae
Jhoskx <jhonatan.ja293@gmail.com>
2024-03-14 23:24:29 -0500

    create form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route,Link, Routes } from 'react-router-dom';
 import Welcom from './Components/Welcom';
+import InitialData from './Components/InitialData';
+
 
 function App() {
   return (
-    <div className="App">
-      <Welcom/>
-    </div>
+<Router>
+  <Routes>
+    <Route path='/Question' Component={InitialData}/>
+    <Route path='/' Component={Welcom}/>
+  </Routes>
+</Router>
+    
   );
 }
 
---
+++ src/Components/InitialData.tsx
@@ -0,0 +1,22 @@
+import { format } from 'path'
+import React from 'react'
+
+const InitialData = () => {
+  return (
+ <div className='image-back'>
+
+    <form action="">
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+
+        <button type='submit'>Send</button>
+    </form>
+ </div>
+  )
+}
+export default InitialData
+
